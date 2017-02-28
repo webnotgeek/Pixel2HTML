07cb10b8083bf3319294688a5f76c3c685fbd44d
Mohamed Hussein <mohamed.hussein@mondiamedia.com>
2017-02-28 16:54:00 +0200

    removing dest from the .ignore file

---
+++ dist/assets/js/main.js
@@ -0,0 +1,47 @@
+/**
+ * Pixel2HTML - 1/1
+ */
+
+var msg = 'Pixel2HTML - 1/1';
+
+function printLog(log){
+    'use strict';
+  return console && console.log(log);
+}
+
+printLog(msg);
+$(".slider > div").slick({
+
+  // normal options...
+  infinite: false,
+  accessibility: true,
+  dots: true,
+
+  // the magic
+  responsive: [{
+
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 3,
+      infinite: true
+    }
+
+  }, {
+
+    breakpoint: 600,
+    settings: {
+      slidesToShow: 2,
+      dots: true
+    }
+
+  }, {
+
+    breakpoint: 300,
+    settings: "unslick" // destroys slick
+
+  }]
+});
+
+
+
+
